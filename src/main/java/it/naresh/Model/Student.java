ef6bd5d4254c214c4610887b90a3de81689f69c9
lordgithup <nagarkuldeep002@gmail.com>
2025-02-18 15:08:55 +0530

    new file well

--- src/main/java/it/naresh/Model/Student.java
+++ src/main/java/it/naresh/Model/Student.java
@@ -12,52 +12,60 @@ import jakarta.persistence.Table;
 @Table(name = "student_dtls")
 public class Student {
 
-	  @Id //there is posible 
- 	  @GeneratedValue(strategy = GenerationType.IDENTITY)
-	  private Long id;
-	  private String firstName;
-	  private String lastName;
-	  
-	  @NaturalId(mutable = true)
-	  private String email;
-	  private String department;
-	  
+	@Id // there is posible
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String deom;
+	@NaturalId(mutable = true)
+	private String email;
+	private String department;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getDepartment() {
 		return department;
 	}
+
 	public void setDepartment(String department) {
 		this.department = department;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Student [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
 				+ ", department=" + department + "]";
 	}
-	  
-	  
+
 }
