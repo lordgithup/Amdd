d1ee29e5e7ee1b9d0ca1e15f68ad9a423b79eba1
lordgithup <nagarkuldeep002@gmail.com>
2025-02-18 12:55:31 +0530

    this student-commiit

--- src/main/java/it/naresh/Model/Student.java
+++ src/main/java/it/naresh/Model/Student.java
@@ -12,8 +12,8 @@ import jakarta.persistence.Table;
 @Table(name = "student_dtls")
 public class Student {
 
-	  @Id
-	  @GeneratedValue(strategy = GenerationType.IDENTITY)
+	  @Id //there is posible 
+ 	  @GeneratedValue(strategy = GenerationType.IDENTITY)
 	  private Long id;
 	  private String firstName;
 	  private String lastName;
